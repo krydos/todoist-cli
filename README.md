52334f644efb567f31460d82b9727b71e951e9b9
Ruslan Bekenev <furyinbox@gmail.com>
2014-02-23 23:07:26 +0200

    add some commands

--- README.md
+++ README.md
@@ -3,10 +3,21 @@ TODOIST-CLI
 
 This is command line tool for [Todoist][1]
 
+Installation
+------------
+
+ - clone this repository to your local machine
+ - add alias to your .bashrc file like **alias t="/path/to/todoist.py"** (optional)
+ - go into your settings in the todoist and copy token field to the clipboard
+ - open todoist.py and paste token from your account to the this file
+
 Command list
 ------------
 
- - **todoist p ls** - get list of projects
+ - **todoist p ls** - get list of all projects
+ - **todoist ls** - get list of all uncompleted tasks
+ - **todoist ls tod** - get list of all uncompleted tasks for today
+ - **todoist ls tom** - get list of all uncompleted tasks for tomorrow
 
 
 Thanks to [@MicahElliott][2] for colortrans.py script
--- todoist.py
+++ todoist.py
@@ -4,6 +4,8 @@ import json
 import colortrans
 import sys
 
+COLOR_OUTPUT = False
+
 class Todoist:
 
     token = "<TOKEN FROM TODOIST USER SETTINGS MENU"
@@ -11,14 +13,28 @@ class Todoist:
 
     """ get all project """
     def getProjects(self):
-        req = urllib2.Request(self.todoist_api_url + 'getProjects?token=' + self.token)
-        response = urllib2.urlopen(req)
-        return response.read()
+        return self._do_get_request(self.todoist_api_url + 'getProjects?token=' + self.token)
 
     """ get all tasks """
-    def getUncompletedTasks(self):
-        # TODO done with this function
-        pass
+    def getUncompletedTasks(self, project_id):
+        return self._do_get_request(self.todoist_api_url + 'getUncompletedItems?token=' + self.token + '&project_id=' + str(project_id))
+
+    """ get all tasks for today """
+    def getTodayTasks(self):
+        return self._do_get_request(self.todoist_api_url + 'query?token=' + self.token + '&queries=["tod"]')
+
+    """ get all tasks for today """
+    def getTomTasks(self):
+        return self._do_get_request(self.todoist_api_url + 'query?token=' + self.token + '&queries=["tom"]')
+
+    """ get all tasks for query """
+    def getQueryTasks(self, query):
+        return self._do_get_request(self.todoist_api_url + 'query?token=' + self.token + '&queries=['+query+']')
+
+    def _do_get_request(self, address):
+        req = urllib2.Request(address)
+        response = urllib2.urlopen(req)
+        return response.read()
 
 
 class IOformat:
@@ -40,6 +56,11 @@ io = IOformat()
 # argument list
 arguments = sys.argv[1:]
 
+# check on color output
+for arg in sys.argv:
+    if arg == '-c':
+        COLOR_OUTPUT = True
+
 try:
 
     # if user want to get list of all projects
@@ -48,12 +69,49 @@ try:
             json_projects = json.loads(todoist.getProjects())
 
             for project in json_projects:
-                io.print_in_color(io.hex2bash(project['color'])[0], project['name'])
+                if COLOR_OUTPUT == True:
+                    io.print_in_color(io.hex2bash(project['color'])[0], project['name'])
+                else:
+                    print project['name']
+
 
     # if user want to get list of all tasks
-    if arguments[0] == 'ls':
-        # TODO get all uncompleted tasks
-        pass
+    if arguments[0] == 'ls' and len(arguments) == 1:
+        json_projects = json.loads(todoist.getProjects())
+        for project in json_projects:
+            json_tasks = json.loads(todoist.getUncompletedTasks(project['id']))
+
+            for task in json_tasks:
+                if COLOR_OUTPUT == True:
+                    io.print_in_color(io.hex2bash(project['color'])[0], task['content'] + ' ('+ project['name'] +')')
+                    print "\n"
+                else:
+                    print task['content'] + ' ('+ project['name'] +')'
+                    print "\n"
+
+    # if user want to get list of all tasks for today
+    if arguments[0] == 'ls' and len(arguments) == 2:
+        if arguments[1] == 'tod':
+            json_tasks = json.loads(todoist.getTodayTasks())
+            json_tasks = json_tasks[0]['data']
+            for task in json_tasks:
+                print task['content']
+                print "\n"
+
+        if arguments[1] == 'tom':
+            json_tasks = json.loads(todoist.getTomTasks())
+            json_tasks = json_tasks[0]['data']
+            for task in json_tasks:
+                print task['content']
+                print "\n"
+
+    if arguments[0] == 'q':
+        json_tasks = json.loads(todoist.getQueryTasks(arguments[1]))
+        json_tasks = json_tasks[0]['data']
+        for task in json_tasks:
+            print task['content']
+            print "\n"
+
 
 except IndexError:
     print 'Something wrong with arguments'
